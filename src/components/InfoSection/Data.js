0b8519b46f74f26470f6df2715983e905a409ba5
vincent <vincentvanderstruis@icloud.com>
2021-06-15 13:55:06 +0200

    push to github

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Earn, exchange & invest.",
+  headline: "Unlimited Transactions with minimal fees",
+  description: "Only you can access your assets. Not us. Not anyone else.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Earn.",
+  headline: "Earn 30%+ interest with top tier DeFi projects.",
+  description:
+    "We have you covered no matter where you are located. Put your assets to work.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/svg-2.svg"),
+  alt: "Piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Sign up now",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get everything set up and ready in under 5 minutes. Deposit crypto-assets in yield protocols like Aave, Compound, and Yearn immediately",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: require("../../images/svg-3.svg"),
+  alt: "Papers",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
